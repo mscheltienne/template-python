3d096bc3a6905922199b47f6b4c4a9e635e969a9
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-02 13:29:31 +0200

    rm noqa F401

--- template/__init__.py
+++ template/__init__.py
@@ -1,4 +1,4 @@
-from . import utils  # noqa: F401
-from ._version import __version__  # noqa: F401
-from .utils.config import sys_info  # noqa: F401
-from .utils.logs import add_file_handler, logger, set_log_level  # noqa: F401
+from . import utils
+from ._version import __version__
+from .utils.config import sys_info
+from .utils.logs import add_file_handler, logger, set_log_level
--- template/utils/__init__.py
+++ template/utils/__init__.py
@@ -1,4 +1,3 @@
 """Utilities module."""
 
-from . import config  # noqa: F401
-from . import logs  # noqa: F401
+from . import config, logs
