baae9164c0c0f2914038115902c9459e63129d5c
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-03 13:52:10 +0200

    fix tutorial

--- tutorials/00_logging.py
+++ tutorials/00_logging.py
@@ -13,7 +13,8 @@ import logging
 from pathlib import Path
 from tempfile import TemporaryDirectory
 
-from template import add_file_handler, logger, set_log_level
+from template import add_file_handler, set_log_level
+from template.utils.logs import logger
 
 # sphinx_gallery_thumbnail_path = '_static/logging/flowchart-light.png'
 
