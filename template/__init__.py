750933239d79077aee61432ac7f3dc57687342eb
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-04-03 13:04:41 +0200

    rm unused imports

--- template/__init__.py
+++ template/__init__.py
@@ -1,4 +1,4 @@
 from . import utils
 from ._version import __version__
 from .utils.config import sys_info
-from .utils.logs import add_file_handler, logger, set_log_level
+from .utils.logs import add_file_handler, set_log_level
