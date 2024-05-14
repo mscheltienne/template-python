4772e4a9bdfd870e71f627dd2c2daf70dd493235
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-05-14 15:02:35 +0200

    add caching of GPU info and test

--- template/utils/config.py
+++ template/utils/config.py
@@ -2,7 +2,7 @@ from __future__ import annotations  # c.f. PEP 563, PEP 649
 
 import platform
 import sys
-from functools import partial
+from functools import lru_cache, partial
 from importlib.metadata import requires, version
 from typing import TYPE_CHECKING
 
@@ -113,13 +113,7 @@ def _list_dependencies_info(
 
             output += f" (backend: {backend})"
         if dep.name == "pyvista":
-            try:
-                from pyvista import GPUInfo
-
-                gi = GPUInfo()
-                version_, renderer = gi.version, gi.renderer
-            except Exception:
-                version_, renderer = None, None
+            version_, renderer = _get_gpu_info()
             if version_ is None:
                 output += " (OpenGL unavailable)"
             else:
@@ -137,3 +131,15 @@ def _list_dependencies_info(
             out(f"✘ Not installed: {', '.join(not_found)}\n")
         else:
             out(f"Not installed: {', '.join(not_found)}\n")
+
+
+@lru_cache(maxsize=1)
+def _get_gpu_info():
+    """Get the GPU information."""
+    try:
+        from pyvista import GPUInfo
+
+        gi = GPUInfo()
+        return gi.version, gi.renderer
+    except Exception:
+        return None, None
