e7ba2bf420e201aaa735e9ca6ebd518d9c697b11
mscheltienne <mathieu.scheltienne@fcbg.ch>
2024-06-12 11:01:14 +0200

    try again to fix D401

--- template/commands/main.py
+++ template/commands/main.py
@@ -6,8 +6,8 @@ from .sys_info import run as sys_info
 
 
 @click.group()
-def run() -> None:
-    """Main package entry-point."""  # noqa: D401
+def run() -> None:  # noqa: D401
+    """Main package entry-point."""
 
 
 run.add_command(sys_info)
