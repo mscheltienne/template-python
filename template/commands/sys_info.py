4b2f0521cc84852e26e6b3ff0cd5e8d727fdef9e
Mathieu Scheltienne <mathieu.scheltienne@fcbg.ch>
2024-06-21 14:44:13 +0200

    add dot

--- template/commands/sys_info.py
+++ template/commands/sys_info.py
@@ -8,7 +8,7 @@ from .. import sys_info
 @click.command(name="sys-info")
 @click.option(
     "--developer",
-    help="Display information for optional dependencies",
+    help="Display information for optional dependencies.",
     is_flag=True,
 )
 def run(developer: bool) -> None:
